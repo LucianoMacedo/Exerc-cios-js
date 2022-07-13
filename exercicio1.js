6966d20cf38e683cf6d57036b41f47892db77bd2
Lu macedo <macedoluck2020@gmail.com>
2022-07-13 13:52:23 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+alert("bem vindo a seguir pediremos alguns dados");
+let name=prompt("Qual seu nome");
+let age=prompt("Qual sua idade");
+let ageConfirm=confirm("sua idade é" + age + " anos");
+alert("Nome : " + name + "\nidade: " + age + " anos")
